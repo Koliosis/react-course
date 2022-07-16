11e0dbd105353d6cf4cf95efd824b06f2b361873
Kole Davis <kstevendavis@gmail.com>
2022-07-16 14:17:58 -0600

    restarted using new react course info

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const el = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(el);
+
 function App() {
-  return <h1>Hi there!</h1>;
-}
+    let message = 'Bye there!'
+    if (Math.random() > .5) {
+        message = 'Hello there!'
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    return <h1>{message}</h1>
+};
+
 root.render(<App />);
